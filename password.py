9713bb52cba472c096aca0960f1db09260af22f1
Alvin Xiang <alvintt@me.com>
2023-08-25 16:03:56 -0700

    new version

--- password.py
+++ password.py
@@ -1,15 +1,17 @@
 password = "a12346"
 i = 3
 while True:
+	i = i - 1
 	pwd = input('Please enter your password:')
 	if pwd == "a123456":
 		print("Login Successful")
 		break
 	else:
-		i = i - 1
-		print("Wrong password !", i, "more left")
-		if i == 0:
-			break
+		print("Wrong password !")
+		if i > 0:
+			print(i, "more left")
+		else:
+			print("No more left")
 
 		
 		
